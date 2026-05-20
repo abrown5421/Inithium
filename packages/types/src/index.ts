57d1ce22b820891cbc3a1ec1931cbd4526a5d330
augustbrown <august.brown@24g.com>
2026-05-20 15:45:03 -0400

    adding in type interfaces for users assets animations etc

---
+++ packages/types/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/animations/animations.types.js'
+export * from './lib/pages/pages.types.js'
+export * from './lib/user/index.js'
---
+++ packages/types/src/lib/assets/asset.types.ts
@@ -0,0 +1,30 @@
+export type AssetCategory = 'image' | 'font' | 'audio' | 'document' | 'other';
+
+export interface AssetBase {
+  filename: string;
+  original_name: string;
+  mimetype: string;
+  size: number;
+  storage_key: string;
+  category: AssetCategory;
+  is_system_asset: boolean;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+}
+
+export interface Asset extends AssetBase {
+  _id: string;
+}
+
+export interface AssetUploadIntent {
+  original_name: string;
+  mimetype: string;
+  size: number;
+  category: AssetCategory;
+}
+
+export interface AssetUploadResponse {
+  asset_id: string;
+  upload_url: string;
+  storage_key: string;
+}
