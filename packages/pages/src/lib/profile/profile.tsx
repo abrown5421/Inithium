e51bcdd493d19a27b78723a7e387973786345fa7
augustbrown <august.brown@24g.com>
2026-06-03 11:51:57 -0400

    quick typo fix

--- packages/pages/src/lib/profile/profile.tsx
+++ packages/pages/src/lib/profile/profile.tsx
@@ -1,7 +1,8 @@
+import { Box } from '@inithium/ui';
 import React from 'react';
 
 const ProfilePage: React.FC = () => {
-  return <div>ProfilePage</div>
+  return <Box padding="md" className="h-full w-full">ProfilePage</Box>
 };
 
 export default ProfilePage
