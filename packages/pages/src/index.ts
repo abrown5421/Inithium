ccbb9b028722accdc2db026e007f928907519509
augustbrown <august.brown@24g.com>
2026-06-18 14:51:37 -0400

    oops

--- packages/pages/src/index.ts
+++ packages/pages/src/index.ts
@@ -5,4 +5,4 @@ export * from './lib/sign-up/index';
 export * from './lib/privacy-policy/index';
 export * from './lib/profile/index'
 
-export * from './lib/documentation-page/index';
+export * from './lib/documentation/index';
